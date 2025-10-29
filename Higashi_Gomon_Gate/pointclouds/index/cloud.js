d7dea25cef1269e7ee1a61a7914352dffb9af9c8
sangij120151-admin <san-j22029@sangi.jp>
2025-10-29 09:59:44 +0900

    Add Potree converted files

---
+++ Higashi_Gomon_Gate/pointclouds/index/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 105838700,
+    "boundingBox": {
+        "lx": -24.812,
+        "ly": -107.818,
+        "lz": -9.273,
+        "ux": 102.62410000000001,
+        "uy": 19.618100000000014,
+        "uz": 118.16310000000002
+    },
+    "tightBoundingBox": {
+        "lx": -24.812,
+        "ly": -107.818,
+        "lz": -9.273,
+        "ux": 78.3,
+        "uy": 19.61800000000001,
+        "uz": 14.309000000000001
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 1.1036289930343629,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
